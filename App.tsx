c4932e8621a535ac2e17bc5828e6d3487121ef51
LeonardoLopeso <l.lopes355@gmail.com>
2023-03-13 00:57:07 -0300

    Created theme

--- App.tsx
+++ App.tsx
@@ -1,8 +1,38 @@
-import React from 'react';
-import { Dashboard } from './src/screens/Dashboard';
+import React, { useEffect } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+import { ThemeProvider } from 'styled-components';
 
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins';
+
+import theme from './src/global/styles/theme';
+
+import { Dashboard } from './src/screens/Dashboard';
+import { Text } from 'react-native';
 
 export default function App() {
-  return <Dashboard />
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  })
+  
+  useEffect(() => {
+    SplashScreen.hideAsync();
+  }, []);
+
+  if (!fontsLoaded) {
+    return <Text>Carregando fontes...</Text>;
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
+  )
 }
 
