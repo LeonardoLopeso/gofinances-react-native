97a1595ed621a7e3a4c7dfcedbfb1ba4d2d20e1c
LeonardoLopeso <l.lopes355@gmail.com>
2023-03-16 21:11:00 -0300

    Header screen Dashboard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
-import { Container, Title }  from './styles';
+
+import { 
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon
+}  from './styles';
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo  source={{ uri:'https://avatars.githubusercontent.com/u/56572789?v=4' }}/>
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Leonardo</UserName>
+            </User>
+          </UserInfo>
+          
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
     </Container>
   )
 }
