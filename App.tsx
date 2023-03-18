08814d1a4c23a42be211caf5da392d71f465cf40
LeonardoLopeso <l.lopes355@gmail.com>
2023-03-18 20:58:39 -0300

    Finalizado listagem de finanças e iniciado form de cadastro

--- App.tsx
+++ App.tsx
@@ -12,6 +12,7 @@ import {
 import theme from './src/global/styles/theme';
 
 import { Dashboard } from './src/screens/Dashboard';
+import { Register } from './src/screens/Register';
 import { Text } from 'react-native';
 
 export default function App() {
@@ -31,7 +32,8 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      {/* <Dashboard /> */}
+      <Register />
     </ThemeProvider>
   )
 }
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+
 import { HighlightCard } from '../../components/HighlightCard';
+import { TransactionCard, TransactionCardProps } from '../../components/TransactionCard';
 
 import { 
   Container,
@@ -13,10 +15,51 @@ import {
   Icon,
   HighlightCards,
   Transactions,
-  Title
+  Title,
+  TransactionList
 }  from './styles';
 
+export interface DataListProps extends TransactionCardProps {
+  id: string;
+}
+
 export function Dashboard() {
+  const data: DataListProps[] = [
+    {
+      id: '1',
+      type: 'positive',
+      title: "Desenvolvimento de site",
+      amount: "R$ 12.000,00",
+      category: {
+        name: "Vendas",
+        icon: 'dollar-sign'
+      },
+      date: "17/03/2023"
+    },
+    {
+      id: '2',
+      type: 'negative',
+      title: "Hamburgueria Pizzy",
+      amount: "R$ 59,00",
+      category: {
+        name: "Alimentação",
+        icon: 'dollar-sign'
+      },
+      date: "14/03/2023"
+    },
+    {
+      id: '3',
+      type: 'negative',
+      title: "Aluguel do apartamento",
+      amount: "R$ 1.200,00",
+      category: {
+        name: "Casa",
+        icon: 'dollar-sign'
+      },
+      date: "14/03/2023"
+    },
+  ]
+
   return (
     <Container>
       <Header>
@@ -56,6 +99,12 @@ export function Dashboard() {
 
       <Transactions>
         <Title>Listagem</Title>
+
+        <TransactionList
+          data={data}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => <TransactionCard data={item} />}
+        />
 
       </Transactions>
     </Container>
