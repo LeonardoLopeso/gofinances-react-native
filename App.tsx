a5ae67872dc1dbb40d6dc25fbe088be1c5ef17c9
LeonardoLopeso <l.lopes355@gmail.com>
2023-03-19 10:53:27 -0300

    Modal de categorias iniciado

--- App.tsx
+++ App.tsx
@@ -13,6 +13,7 @@ import theme from './src/global/styles/theme';
 
 import { Dashboard } from './src/screens/Dashboard';
 import { Register } from './src/screens/Register';
+import { CategorySelect } from './src/screens/CategorySelect';
 import { Text, View } from 'react-native';
 
 export default function App() {
@@ -37,7 +38,8 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       {/* <Dashboard /> */}
-      <Register />
+      {/* <Register /> */}
+      <CategorySelect />
     </ThemeProvider>
   )
 }
